4a77d71f8d7e94dffdada749b4e5cfdb2ac12295
benhug2 <benhug2@illinosi.edu>
2024-04-24 14:17:40 -0500

    creating rocket toml

---
+++ Project/target/debug/deps/gemm_f32-5a8ee87f90bd7dea.d
@@ -0,0 +1,7 @@
+C:\Users\benhug2\cs128\CS128H-Project\Project\target\debug\deps\libgemm_f32-5a8ee87f90bd7dea.rmeta: C:\Users\benhug2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\gemm-f32-0.17.1\src\lib.rs C:\Users\benhug2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\gemm-f32-0.17.1\src\gemm.rs C:\Users\benhug2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\gemm-f32-0.17.1\src\microkernel.rs
+
+C:\Users\benhug2\cs128\CS128H-Project\Project\target\debug\deps\gemm_f32-5a8ee87f90bd7dea.d: C:\Users\benhug2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\gemm-f32-0.17.1\src\lib.rs C:\Users\benhug2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\gemm-f32-0.17.1\src\gemm.rs C:\Users\benhug2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\gemm-f32-0.17.1\src\microkernel.rs
+
+C:\Users\benhug2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\gemm-f32-0.17.1\src\lib.rs:
+C:\Users\benhug2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\gemm-f32-0.17.1\src\gemm.rs:
+C:\Users\benhug2\.cargo\registry\src\index.crates.io-6f17d22bba15001f\gemm-f32-0.17.1\src\microkernel.rs:
---
+++ Rocket/target/debug/deps/rock-8f845c4675996b68.d
@@ -0,0 +1,5 @@
+C:\Users\benhug2\cs128\CS128H-Project\Rocket\target\debug\deps\librock-8f845c4675996b68.rmeta: src/rocket.rs
+
+C:\Users\benhug2\cs128\CS128H-Project\Rocket\target\debug\deps\rock-8f845c4675996b68.d: src/rocket.rs
+
+src/rocket.rs:
